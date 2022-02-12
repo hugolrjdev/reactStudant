0596b7cf25c20292e295f12d36de12aa63d5f03e
hugolrjdev <fortalezaplanodesaude@gmail.com>
2022-02-12 11:24:22 -0300

    cssStyle

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -14,15 +14,15 @@ export default props =>
         <div className='app'>
            <h1>Fundamentos React</h1>
             <div className="cards">
-            <Card titulo="#04 - Aleatorios"> 
+            <Card titulo="#04 - Aleatorios" color="#fe8a71"> 
                 <Aleatorio min={1} max={10} />
             </Card>
                 
-            <Card titulo="#03 - Fragmentos">
+            <Card titulo="#03 - Fragmentos" color="#f6cd61">
                 <Fragmento />
             </Card>
         
-            <Card titulo="#02 - Com parametros">
+            <Card titulo="#02 - Com parametros" color="#3da4ab ">
                 <ComParamentros 
                     titulo="Situação do Aluno"
                     aluno={user}
@@ -30,8 +30,12 @@ export default props =>
                 />
             </Card>
         
-            <Card titulo="#01 Primeiro Exercicio">
+            <Card titulo="#01 Primeiro Exercicio" color="#0e9aa7">
                 <Primeiro></Primeiro>
+            </Card>
+
+            <Card titulo="proximo" color="#4a4e4d ">
+
             </Card>
         </div>
     </div>
--- fundamentos-react/src/componentes/layout/Card.jsx
+++ fundamentos-react/src/componentes/layout/Card.jsx
@@ -2,9 +2,14 @@ import './Card.css';
 import React from 'react';
 
 export default function Card(props) {
+    const cardStyleColor = {
+        backgroundColor: props.color || '#f00',
+        borderColor:  props.color || '#f00',
+    }
+    
     return(
         <div>
-            <div className='card'>
+            <div className='card' style={cardStyleColor}>
                 <div className='title'>{props.titulo}</div>
                 <div className='content'>{props.children}</div>
             </div>
